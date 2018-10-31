cfd552dce682962486529c3f4be04ca29911d79e
zut <ahilton@gmail.com>
2018-11-01 08:33:12 +1100

    init

---
+++ src/main/java/com/zut/galaweb/GalaWebApplication.java
@@ -0,0 +1,12 @@
+package com.zut.galaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalaWebApplication.class, args);
+	}
+}
